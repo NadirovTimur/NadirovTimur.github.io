3693994391cde90f277a3bfe10c2ca794ce9523c
NadirovTimur <98875793+NadirovTimur@users.noreply.github.com>
2022-05-02 23:36:17 +0700

    Add files via upload

--- src/js/myScript.js
+++ src/js/myScript.js
@@ -1,65 +1,8 @@
 $(document).ready(function () {
-
-    $(window).scroll(() => {
-        let scrollDistance = $(window).scrollTop();
-
-        $(".section").each((u, el) => {
-
-            if ($(el).offset().top - $("nav").outerHeight() <= scrollDistance) {
-                $("nav a").each((i, el) => {
-                    if ($(el).hasClass("active")) {
-                        $(el).removeClass("active");
-                    }
-                });
-
-                $('nav li:eq('+ i +')').find('a').addClass('active');
-            }
-
-        });
-
+    $('.image-link').magnificPopup({
+        type: 'image'
     });
-
-    $(document).ready(function() {
-  $('.image-link').magnificPopup({type:'image'});
 });
-    
-});
-
-let type = prompt("Выберите тип сайта: 1 - корпоративный 2 - интернет магазин");
-let design = prompt("Выберите тип сайта: 1 - красивый 2 - очень красивый");
-let adapt = prompt("Выберите тип сайта: 1 - адаптивный 2 - неадаптивный");
-
-let p1, p2, p3, t1, t2, t3 = 0;
-
-if (type = 1) {
-    p1 = 1000;
-    t1 = 1;
-} 
-else {
-    p1 = 2000;
-    t1 = 2;
-}
-if (design = 1) {
-    p2 = 1000;
-    t2 = 1;
-} 
-else {
-    p2 = 2000;
-    t2 = 2;
-}
-if (adapt = 1) {
-    p3 = 2000;
-    t3 = 2;
-} 
-else {
-    p3 = 1000;
-    t3 = 1;
-}
-
-let pric = p1 + p2 + p3;
-let term = t1 + t2 + t3;
-
-alert("Cтоимость сайта:" + pric + "руб. " + " Срок:" + term + "суток");
 
 $('a[href^="#"]').click(function () {
     let valHref = $(this).attr("href");
@@ -68,25 +11,139 @@ $('a[href^="#"]').click(function () {
     });
 });
 
-/*
+
+/* здесь чето навигация активной не становится*/
+$(window).scroll(() => {
+    let scrollDistance = $(window).scrollTop();
+
+    $("section").each((i, el) => {
+
+        if ($(el).offset().top - $("nav").outerHeight() <= scrollDistance) {
+            $("nav a").each((i, el) => {
+                if ($(el).hasClass("active")) {
+                    $(el).removeClass("active");
+                }
+            });
+
+            $('nav li:eq(' + i + ')').find('a').addClass('active');
+        }
+
+    });
+
+});
+
+
+
+
+var type = prompt("Выберите тип сайта: 1 - корпоративный 2 - интернет-магазин");
+var design = prompt("Выберите тип сайта: 1 - красивый 2 - очень красивый");
+var adapt = prompt("Выберите тип сайта: 1 - адаптивный 2 - неадаптивный");
+
+var p1, p2, p3, t1, t2, t3 = 0;
+
+if (type == 1) {
+    p1 = 1000;
+    t1 = 1;
+} else if (type == 2) {
+    p1 = 2000;
+    t1 = 2;
+}
+if (design == 1) {
+    p2 = 1000;
+    t2 = 1;
+} else if (design == 2) {
+    p2 = 2000;
+    t2 = 2;
+}
+if (adapt == 1) {
+    p3 = 2000;
+    t3 = 2;
+} else if (adapt == 2) {
+    p3 = 1000;
+    t3 = 1;
+}
+
+var pric = p1 + p2 + p3;
+var term = t1 + t2 + t3;
+
+
+alert("Cтоимость сайта:" + pric + "руб. " + " Срок:" + term + "суток");
+
+
+$('#js-button').click(function () {
+    var value = $('#type option:selected').text();
+    var value1 = $('#design option:selected').text();
+    var value2 = $('#adapt option:selected').text();
+
+    var p1, p2, p3, t1, t2, t3 = 0;
+
+    if (value == "корпоративный") {
+        p1 = 1000;
+        t1 = 1;
+    }
+    if (value == "интернет-магазин") {
+        p1 = 2000;
+        t1 = 2;
+    }
+    if (value1 == "красивый") {
+        p2 = 1000;
+        t2 = 1;
+    }
+    if (value1 == "очень красивый") {
+        p2 = 2000;
+        t2 = 2;
+    }
+    if (value2 == "адаптивный") {
+        p3 = 2000;
+        t3 = 2;
+    }
+    if (value2 == "неадаптивный") {
+        p3 = 1000;
+        t3 = 1;
+    }
+
+    var pric = p1 + p2 + p3;
+    var term = t1 + t2 + t3;
+
+    $('#js-result').html(pric);
+    $('#js-result2').html(term);
+});
+
+
+
+
+
+
 let options = {threshold: [1]};
 let observer = new IntersectionObserver(onEntry, options);
 let elements = $('.count');
 elements.each((i,el) => {
     observer.observe(el);
 });
-*/
 
-$('.count').each(function onEntry (entry) {
+function onEntry (entry){
+entry.forEach(change => {
+if(change.isIntersecting){
+$('.count').each(function () {
     $(this).prop('Counter', 0).animate({
-            Counter: $(this).data('value')
-        }, {
+        Counter: $(this).data('value')
+    }, {
         duration: 10000,
         easing: 'swing',
-        step: function (now) {                      
+        step: function (now) {
             $(this).text(Math.ceil(now));
         }
     });
 });
+}
+})
+}
+
+
+
+
+
+
+
 
 
